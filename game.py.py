1572125e77ae58abce594fea356e8ee508891626
Vishnn Mali <76401790+Vishnu-yes-i-am@users.noreply.github.com>
2021-05-22 22:02:58 +0530

    Update game.py.py

--- game.py.py
+++ game.py.py
@@ -3,6 +3,7 @@ from pygame import mixer
 from playsound import playsound
 import random
 import time
+clock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption("Car_Racing By VISHNU")
 display=pygame.display.set_mode((1200,720))
@@ -12,17 +13,21 @@ black=pygame.Color(0, 0, 0)
 green=pygame.Color(98, 244, 68)
 blue=pygame.Color(111, 148, 191)
 red=pygame.Color(250, 0, 0)
+#------------------
+cars=[r"car1.jpg",r"car2.jpg",r"car3.jpg",r"car4.jpg",r"car5.jpg",r"car6.jpg",r"car7.jpg",r"car8.jpg",r"car9.jpg"]
 font=pygame.font.SysFont("Algerian", 50)
 newgame=True
-highscore=0
+f=open("highscore.txt",'r')
+k=f.readlines()
+highscore=int(k[0])
 mixer.init()
 mixer.music.load("theme.wav")
+car=pygame.transform.rotate(pygame.image.load(random.choice(cars)),90)
 while newgame:
     display.fill(blue)
     display.blit(font.render("Starting The Game....", True, red), (200,400))
     pygame.display.update()
     pygame.time.wait(2*1000)
-    # playsound("theme.wav",False)
     mixer.music.play()
     y=0
     y1=200
@@ -30,21 +35,23 @@ while newgame:
     y3=600
     xx=220
     yy=500
-    bx=200
+    bx=bx=random.choice([200,400,600])
     r=1
-    by=0
+    by=-100
     score=0
     speed=7
     rect1=pygame.Rect(812, 0, 400, 780)
     rect2=pygame.Rect(0, 0, 190, 780)
     gameover=False
     while not gameover:
+        clock.tick(60)
         y+=speed
         y1+=speed
         y2+=speed
         y3+=speed
-        by+=speed
+        by+=speed-3
         if score>5*r:
+            playsound("horn.wav",False)
             speed+=2
             r+=1
         if y>700:
@@ -65,10 +72,11 @@ while newgame:
             y3=-100
         display.fill(white)
         if by>700:
-            by=0
+            by=-100
             score+=1
             bx=random.choice([200,400,600])
-        pic2=pygame.transform.scale(pygame.image.load("block.jpg"), (200,100))
+            car=pygame.transform.rotate(pygame.image.load(random.choice(cars)),90)
+        pic2=pygame.transform.scale(car, (180,270))
         display.blit(pic2,(bx,by))
         pygame.draw.line(display, black, (200,y), (200,y+130),16)
         pygame.draw.line(display, black, (200,y1), (200,y1+130),16)
@@ -86,26 +94,28 @@ while newgame:
         pygame.draw.line(display, black, (800,y1), (800,y1+130),16)
         pygame.draw.line(display, black, (800,y2), (800,y2+130),16)
         pygame.draw.line(display, black, (800,y3), (800,y3+130),16)
-        # pygame.draw.rect(display, green, (820,80,180,100))
-        pic=pygame.image.load(r"C:\Users\vishn\Desktop\development zone\car\car2.jpg")
+        pic=pygame.image.load("car.jpg")
         pic=pygame.transform.rotate(pic, 270)
         pic=pygame.transform.scale(pic, (160,200))
         display.blit(pic, (xx,yy))
         pygame.draw.rect(display, green, rect1)
         pygame.draw.rect(display, green, rect2)
         display.blit(font.render("Score: "+str(score),True, black),(850,100))
+        display.blit(font.render("High Score: "+str(highscore),True, black),(830,230))
         pygame.display.update()
         if pygame.Rect(bx,by,200,100).colliderect((xx,yy,160,200)):
             speed=0
             mixer.music.stop()
             playsound("boom.wav")
             gameover=True
-            display.blit(font.render("Game Over",True, green),(300,400))
+            display.blit(font.render("Game Over",True, red),(300,400))
             pygame.display.update()
             pygame.time.wait(3000)
             display.fill(blue)
-            display.blit(font.render("your score is :"+str(score),True, green),(200,400))
+            display.blit(font.render("your score is :"+str(score),True, red),(200,400))
             if score>highscore:
+                f2=open("highscore.txt",'w')
+                f2.write(str(score))
                 display.blit(font.render("Great you had beaten the high score",True, green),(150,200))
             highscore=max(score,highscore)
             display.blit(font.render("press 'y' to play again else 'n' to quit.",True, green),(50,600))
@@ -129,15 +139,17 @@ while newgame:
                 if el.key==pygame.K_UP and yy>100:
                     yy-=50
                     break
-                elif el.key==pygame.K_DOWN and yy<500:
+                if el.key==pygame.K_DOWN and yy<500:
                     yy+=50
                     break
-                elif el.key==pygame.K_RIGHT and xx<600:
+                if el.key==pygame.K_RIGHT and xx<600:
                     xx+=200
                     break
-                elif el.key==pygame.K_LEFT and xx>300:
+                if el.key==pygame.K_LEFT and xx>300:
                     xx-=200
                     break
+                if el.key==pygame.K_SPACE:
+                    playsound('classic.wav',False) 
             elif el.type==pygame.QUIT:
                 pygame.quit()
                 quit()
